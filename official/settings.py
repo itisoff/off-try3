2b12dc73c8284f9e78df29fc579a380f900a5542
itisoff <anikaeraera@gmail.com>
2021-08-27 14:11:43 +0600

    edited O

--- official/settings.py
+++ official/settings.py
@@ -75,22 +75,22 @@ WSGI_APPLICATION = 'official.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd2djgs43kcp9rb',
-        'USER': 'wyfxiiwofuwryk',
-        'PASSWORD': 'c226fa44542ade7cb47dd20ce7108e93e2298642131597a3de5d8e599f4cc45b',
-        'HOST': 'ec2-44-196-8-220.compute-1.amazonaws.com',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default':{
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'd2djgs43kcp9rb',
+#         'USER': 'wyfxiiwofuwryk',
+#         'PASSWORD': 'c226fa44542ade7cb47dd20ce7108e93e2298642131597a3de5d8e599f4cc45b',
+#         'HOST': 'ec2-44-196-8-220.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
